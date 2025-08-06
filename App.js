0b7a622ebf73a2e1441ce367cafbb511869a6265
Lakshantha Dissanayake <lakshanthad@gmail.com>
2025-08-05 19:30:14 -0700

    Remove reduntant import

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 // App.js
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
